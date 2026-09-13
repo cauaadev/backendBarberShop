dd1b744850d41af5e6bb9351b1d9538d37b2fb44
Caua Diniz <cauasouzadev@gmail.com>
2026-09-13 16:18:35 -0300

    Change classes type to records in DTOs

--- src/main/java/com/corteBrabo/barbershopApi/security/CustomUserDetailsService.java
+++ src/main/java/com/corteBrabo/barbershopApi/security/CustomUserDetailsService.java
@@ -21,7 +21,6 @@ public class CustomUserDetailsService implements UserDetailsService {
     public UserDetails loadUserByUsername(String telefone) throws UsernameNotFoundException {
         User user = userRepository.findByTelefone(telefone)
                 .orElseThrow(() -> new UsernameNotFoundException("Usuário não encontrado: " + telefone));
-
         if (user.getRole().equals(UserRole.CLIENT)) {
             throw new UsernameNotFoundException("Cliente não tem acesso ao sistema");
         }
